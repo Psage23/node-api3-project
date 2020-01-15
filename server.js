65873d0e49244bcf39b18219a2d598638fabf6ee
Psage23 <pamelasage3@gmail.com>
2020-01-15 16:35:27 -0700

    server is working

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const router = require('./posts/postRouter');
 const server = express();
+
+server.use(logger);
+server.use(express.json());
+server.use('/', router);
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +14,12 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const {method, originalUrl} = req;
+  console.log(`${method} to ${originalUrl}`);
+
+  next();
+}
+
 
 module.exports = server;
